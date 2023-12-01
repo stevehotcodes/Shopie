a3b3d230d57ee1526a6b37b407ca7f49a1998402
annanodiyo <annanodiyo@gmail.com>
2023-12-01 10:16:14 +0300

    designed the product cards

--- FRONTEND-NG/src/app/userdashboard/userdashboard.component.ts
+++ FRONTEND-NG/src/app/userdashboard/userdashboard.component.ts
@@ -6,51 +6,55 @@ import { CartService } from '../services/cart.service';
 @Component({
   selector: 'app-userdashboard',
   templateUrl: './userdashboard.component.html',
-  styleUrls: ['./userdashboard.component.css']
+  styleUrls: ['./userdashboard.component.css'],
 })
 export class UserdashboardComponent {
+  showModal = false;
+  selectedProduct: any;
 
+  orders: IorderDetailsWithUserInfo[] = [];
+  products: any[] = [];
+  constructor(
+    private orderSvc: OrdersService,
+    private productSvc: ProductsService,
+    private cartSvc: CartService
+  ) {}
 
-  orders:IorderDetailsWithUserInfo[]=[]
-  products:any[]=[]
-  constructor(private orderSvc:OrdersService, private productSvc:ProductsService,private cartSvc:CartService){}
-
-  ngOnInit(){
-      this.getAllOrders()
-      this.getAllProducts()
-  }
-      
-
-  getAllOrders(){
-    this.orderSvc.getOrdersByUser().subscribe(
-      res=>{
-        console.log(res)
-        this.orders=res
-        console.log(this.orders)
-      }
-    )
+  ngOnInit() {
+    this.getAllOrders();
+    this.getAllProducts();
   }
 
-  getAllProducts(){
-    this.productSvc.getAllProducts().subscribe(
-      res=>{
-        console.log("all products",res)
-        this.products=res
-      }
-    )
-  }
-  addToCart(id:string){
-   
-    this.cartSvc.addItem(id).subscribe(
-      res=>{
-        console.log(id);
-        
-        console.log("res fronm service",res);
-        
-      }
-    )
-    console.log("item added to  works");
-    
+  openModal(product: any) {
+    this.selectedProduct = product;
+    this.showModal = true;
   }
 
+  closeModal() {
+    this.showModal = false;
+    this.selectedProduct = null;
+  }
+
+  getAllOrders() {
+    this.orderSvc.getOrdersByUser().subscribe((res) => {
+      console.log(res);
+      this.orders = res;
+      console.log(this.orders);
+    });
+  }
+
+  getAllProducts() {
+    this.productSvc.getAllProducts().subscribe((res) => {
+      console.log('all products', res);
+      this.products = res;
+    });
+  }
+  addToCart(id: string) {
+    this.cartSvc.addItem(id).subscribe((res) => {
+      console.log(id);
+
+      console.log('res fronm service', res);
+    });
+    console.log('item added to  works');
+  }
 }
